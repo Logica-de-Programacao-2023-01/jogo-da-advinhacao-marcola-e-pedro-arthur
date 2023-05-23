b4511ef324803e97dca2eb2a84216e3c5af033b8
Hu0no <126472671+Hu0no@users.noreply.github.com>
2023-05-23 10:35:47 -0300

    Update jogo.go

--- jogo.go
+++ jogo.go
@@ -18,8 +18,8 @@ func main() {
 		var tentativas int = 0
 		x := rand.Intn((max - min) + min)
 		var n int
+		fmt.Println("--------------------------------")
 		for n != x {
-			fmt.Println("--------------------------------")
 			fmt.Println("Insira um número entre 1 e 100")
 			tentativas++
 			fmt.Scan(&n)
@@ -28,6 +28,7 @@ func main() {
 			} else if n > x {
 				fmt.Println("O número é menor que ", n)
 			} else {
+				fmt.Println("--------------------------------")
 				fmt.Println("Parabéns, você acertou!")
 				fmt.Println("Você utilizou ", tentativas, " tentativas")
 				slice_tentativas = append(slice_tentativas, tentativas)
@@ -37,6 +38,7 @@ func main() {
 		fmt.Println("Você deseja jogar novamente? (sim ou não)")
 		fmt.Scan(&jogar_denovo)
 		if jogar_denovo == "não" {
+			fmt.Println("--------------------------------")
 			num := 0
 			for num < len(slice_tentativas) {
 				fmt.Printf("Você utilizou %d tentativas na %dª jogada\n", slice_tentativas[num], jogadas[num])
